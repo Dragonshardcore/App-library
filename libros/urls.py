a35cebefc13bf50848c98bbbe45a1e8b7046b141
Charles Darwin <carlos.campos@uisek.edu.ec>
2026-02-01 16:54:37 -0500

    Backend culminado comentado y con un README

--- libros/urls.py
+++ libros/urls.py
@@ -2,10 +2,15 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import LibroViewSet, ReservaViewSet, AutorViewSet
 
+# Router de DRF
 router = DefaultRouter()
+
+# Registro de rutas automáticas
 router.register(r'libros', LibroViewSet)
 router.register(r'reservas', ReservaViewSet)
 router.register(r'autores', AutorViewSet)
+
+# URLs finales de la API
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- libros/views.py
+++ libros/views.py
@@ -2,17 +2,24 @@ from rest_framework import viewsets
 from .models import Autor, Libro, Reserva
 from .serializers import AutorSerializer, LibroSerializer, ReservaSerializer
 
-
+# VIEWSET AUTOR
 class AutorViewSet(viewsets.ModelViewSet):
+     # Obtiene todos los autores
     queryset = Autor.objects.all()
+     # Usa el serializer de Autor
     serializer_class = AutorSerializer
 
+# VIEWSET LIBRO
 
 class LibroViewSet(viewsets.ModelViewSet):
+     # Obtiene todos los libros
     queryset = Libro.objects.all()
+     # Usa el serializer de Libro
     serializer_class = LibroSerializer
 
-
+# VIEWSET RESERVA
 class ReservaViewSet(viewsets.ModelViewSet):
+     # Obtiene todas las reservas
     queryset = Reserva.objects.all()
+     # Usa el serializer de Reserva
     serializer_class = ReservaSerializer
